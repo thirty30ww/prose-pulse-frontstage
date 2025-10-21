e866e732f35ab27d3f14d8207bc794431c87e01c
thirty30ww <2834379272@qq.com>
2025-10-21 20:02:20 +0800

    修改默认端口为5174

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 5174,
+    host: '0.0.0.0',
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080', // 后端端口
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
